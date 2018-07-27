e4db7c1ef27f55b34b32a59ec66e6f5c0672957b
ArnaudVallette <vallette.arnaud@gmail.com>
2018-07-27 18:47:38 +0200

    WIP reponse service

--- src/app/Services/reponse.service.ts
+++ src/app/Services/reponse.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment as ENV} from '../../environments/environment';
+import { Reponse } from '../reponse';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReponseService {
+  private apiURL: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.apiURL = ENV.apiUrl + '/reponse';
+  }
+
+  createReponseNo(reponse: Reponse) : Observable<Reponse> {
+    const url = this.apiURL + '/1';
+    return this.http.post<Reponse>(url, reponse);
+  }
 }
