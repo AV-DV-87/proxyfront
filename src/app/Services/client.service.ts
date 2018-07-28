bfd673ce85b55b9391f90ae1ce6c003dc53d5441
ArnaudVallette <vallette.arnaud@gmail.com>
2018-07-28 19:33:43 +0200

    WIP comment répcupérer la réponse numero client pour attrib à une variable boolean

--- src/app/Services/client.service.ts
+++ src/app/Services/client.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import {environment as ENV} from '../../environments/environment';
+import { environment as ENV } from '../../environments/environment';
 import { Client } from '../client';
 import { Observable, Subject } from '../../../node_modules/rxjs';
 import { Reponse } from '../reponse';
@@ -12,39 +12,39 @@ import { HttpResponse } from '../../../node_modules/@types/selenium-webdriver/ht
 export class ClientService {
 
   private apiUrl: string;
-  private clientNumber:Client;
-  private found:boolean;
+  private clientNumber: Client;
+  private found: boolean;
 
 
   constructor(private httpClient: HttpClient) {
     this.apiUrl = ENV.apiUrl + '/client';
   }
 
-  creat(client:Client): Observable<Client>{
+  creat(client: Client): Observable<Client> {
     let result = new Subject<Client>();
     this.httpClient.post<Client>(this.apiUrl, client)
-    .subscribe((newClient)=>{
-      result.complete();
-    }, (response: HttpErrorResponse) => {
-      result.error(response.message);
-    });
+      .subscribe((newClient) => {
+        result.complete();
+      }, (response: HttpErrorResponse) => {
+        result.error(response.message);
+      });
 
     return result;
   }
 
- find(client: Client): boolean{
-   let found: boolean;
-   let result = new Subject<string>();
-   this.httpClient.get<string>(this.apiUrl + '/find/'+ client.numero)
-   .subscribe((rep)=>{
-    //  this.found = rep;
-     console.log(found);
-     result.complete();
-   }, (response : HttpErrorResponse) => {
-    result.error(response.message);
-  });
-  console.log(found);
-  return found;
-}
+  find(client: Client): Subject<boolean> {
+    
+    let result = new Subject<boolean>();
+    this.httpClient.get(this.apiUrl + '/find/' + client.numero, {responseType: 'text'})    
+    .subscribe((rep) => {
+        // console.log(rep);
+        
+        result.complete();
+      }, (response: HttpErrorResponse) => {
+        result.error(response.message);
+      });
+    
+    return result;
+  }
 
 }
--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 import { Client } from '../client';
 import { NgForm } from '@angular/forms';
 import { ClientService } from '../Services/client.service';
+import { Subject } from 'rxjs';
 
 
 
@@ -18,6 +19,7 @@ export class ClientComponent implements OnInit {
   //@Output() onFind: EventEmitter<string>
   private model: Client;
   private clientNum: Client;
+  public found : boolean;
 
   constructor(private service: ClientService) {
     this.model = new Client();
@@ -42,7 +44,13 @@ export class ClientComponent implements OnInit {
   submitNum(form: NgForm){
     let data: Client = JSON.parse(JSON.stringify(this.clientNum));
     //this.onFind.emit(data);
-    this.service.find(data);
+    
+    this.service.find(data).subscribe(
+      (toto) =>{ this.found = toto;
+        console.log(this.found);
+      }
+    );
+    console.log(this.found);
     form.resetForm();
   }
 
